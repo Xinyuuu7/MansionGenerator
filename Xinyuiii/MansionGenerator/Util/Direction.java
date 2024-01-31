c60f5b01b1937331b98caf7b6d2602ac7a20f9ad
Xinyuuu7 <124234708+Xinyuuu7@users.noreply.github.com>
2024-01-31 09:49:53 +0800

    Update Direction.java

--- Xinyuiii/MansionGenerator/Util/Direction.java
+++ Xinyuiii/MansionGenerator/Util/Direction.java
@@ -1,6 +1,8 @@
 package Xinyuiii.MansionGenerator.Util;
 
-import com.seedfinding.mccore.util.pos.BPos;
+import com.google.common.collect.Iterators;
+import com.seedfinding.mccore.util.math.Vec3i;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -8,22 +10,22 @@ import java.util.Iterator;
 import java.util.function.Predicate;
 
 public enum Direction{
-    DOWN(0, 1, -1, "down", AxisDirection.NEGATIVE, Axis.Y, new BPos(0, -1, 0)),
-    UP(1, 0, -1, "up", AxisDirection.POSITIVE, Axis.Y, new BPos(0, 1, 0)),
-    NORTH(2, 3, 2, "north", AxisDirection.NEGATIVE, Axis.Z, new BPos(0, 0, -1)),
-    SOUTH(3, 2, 0, "south", AxisDirection.POSITIVE, Axis.Z, new BPos(0, 0, 1)),
-    WEST(4, 5, 1, "west", AxisDirection.NEGATIVE, Axis.X, new BPos(-1, 0, 0)),
-    EAST(5, 4, 3, "east", AxisDirection.POSITIVE, Axis.X, new BPos(1, 0, 0));
+    DOWN(0, 1, -1, "down", AxisDirection.NEGATIVE, Axis.Y, new Vec3i(0, -1, 0)),
+    UP(1, 0, -1, "up", AxisDirection.POSITIVE, Axis.Y, new Vec3i(0, 1, 0)),
+    NORTH(2, 3, 2, "north", AxisDirection.NEGATIVE, Axis.Z, new Vec3i(0, 0, -1)),
+    SOUTH(3, 2, 0, "south", AxisDirection.POSITIVE, Axis.Z, new Vec3i(0, 0, 1)),
+    WEST(4, 5, 1, "west", AxisDirection.NEGATIVE, Axis.X, new Vec3i(-1, 0, 0)),
+    EAST(5, 4, 3, "east", AxisDirection.POSITIVE, Axis.X, new Vec3i(1, 0, 0));
 
     private final int index;
     private final int opposite;
     private final int horizontalIndex;
     private final Axis axis;
-    private final BPos directionVec;
+    private final Vec3i directionVec;
     private static final Direction[] VALUES = values();
     private static final Direction[] BY_INDEX = Arrays.stream(VALUES).sorted(Comparator.comparingInt((a) -> a.index)).toArray((b) -> new Direction[b]);
     private static final Direction[] BY_HORIZONTAL_INDEX = Arrays.stream(VALUES).filter((a) -> a.getAxis().isHorizontal()).sorted(Comparator.comparingInt((b) -> b.horizontalIndex)).toArray((c) -> new Direction[c]);
-    Direction(int indexIn, int oppositeIn, int horizontalIndexIn, String nameIn, AxisDirection axisDirectionIn, Axis axisIn, BPos directionVecIn) {
+    Direction(int indexIn, int oppositeIn, int horizontalIndexIn, String nameIn, AxisDirection axisDirectionIn, Axis axisIn, Vec3i directionVecIn) {
         this.index = indexIn;
         this.horizontalIndex = horizontalIndexIn;
         this.opposite = oppositeIn;
@@ -129,15 +131,16 @@ public enum Direction{
     }
 
     public enum Plane implements Iterable<Direction>, Predicate<Direction> {
-        HORIZONTAL(new Direction[]{Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST}, new Axis[]{Axis.X, Axis.Z}),
-        VERTICAL(new Direction[]{Direction.UP, Direction.DOWN}, new Axis[]{Axis.Y});
+        HORIZONTAL(new Direction[]{Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST}, new Axis[]{Axis.X, Axis.Z});
+        private final Direction[] facingValues;
 
-        Plane(Direction[] facingValuesIn, Axis[] axisValuesIn) {
+        Plane(Direction[] facingValuesIn, Direction.Axis[] axisValuesIn) {
+            this.facingValues = facingValuesIn;
         }
 
         @NotNull
         public Iterator<Direction> iterator() {
-            return null;
+            return Iterators.forArray(this.facingValues);
         }
 
         public boolean test(Direction direction) {
