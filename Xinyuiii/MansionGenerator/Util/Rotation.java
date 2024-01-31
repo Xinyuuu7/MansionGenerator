c74818707f428ae212915dff364a4407fe1c818c
Xinyuuu7 <124234708+Xinyuuu7@users.noreply.github.com>
2024-01-31 09:20:43 +0800

    Update Rotation.java

--- Xinyuiii/MansionGenerator/Util/Rotation.java
+++ Xinyuiii/MansionGenerator/Util/Rotation.java
@@ -2,19 +2,13 @@ package Xinyuiii.MansionGenerator.Util;
 
 import com.seedfinding.mccore.rand.ChunkRand;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public enum Rotation {
     NONE(Direction.NORTH),
     CLOCKWISE_90(Direction.EAST),
     CLOCKWISE_180(Direction.SOUTH),
     COUNTERCLOCKWISE_90(Direction.WEST);
 
-    private final Direction direction;
-
     Rotation(Direction direction) {
-        this.direction = direction;
     }
 
     public Rotation add(Rotation rotation) {
@@ -57,10 +51,6 @@ public enum Rotation {
         }
     }
 
-    public Direction getDirection() {
-        return this.direction;
-    }
-
     public Direction rotate(Direction facing) {
         if (facing.getAxis() == Direction.Axis.Y) {
             return facing;
@@ -77,27 +67,8 @@ public enum Rotation {
             }
         }
     }
-
-    public int rotate(int a, int b) {
-        switch(this) {
-            case CLOCKWISE_90:
-                return (a + b / 4) % b;
-            case CLOCKWISE_180:
-                return (a + b / 2) % b;
-            case COUNTERCLOCKWISE_90:
-                return (a + b * 3 / 4) % b;
-            default:
-                return a;
-        }
-    }
-
+    
     public static Rotation getRandom(ChunkRand rand) {
         return values()[rand.nextInt(values().length)];
-    }
-
-    public static List<Rotation> shuffledRotations(ChunkRand rand) {
-        List<Rotation> list = new ArrayList<>(List.of(values()));
-        Util.shuffle(list, rand);
-        return list;
     }
 }
