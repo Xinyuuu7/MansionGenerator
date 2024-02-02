5da4214d135b9905b6b9340047c94caa759e1aee
Xinyuuu7 <124234708+Xinyuuu7@users.noreply.github.com>
2024-02-02 08:45:53 +0800

    Update Main.java

--- src/main/java/Xinyuiii/MansionGenerator/properties/Main.java
+++ src/main/java/Xinyuiii/MansionGenerator/properties/Main.java
@@ -10,25 +10,21 @@ import com.seedfinding.mcfeature.loot.item.ItemStack;
 import java.util.List;
 
 public class Main {
-
     public static final MCVersion version = MCVersion.v1_20;
 
     public static void main(String[] args) {
         test();
     }
 
-
     public static void test() {
         ChunkRand rand = new ChunkRand();
         long seed = -1432629000L;
         CPos pos = (new BPos(-5488, 0, 14624)).toChunkPos();
-
         MansionGenerator generator = new MansionGenerator(version);
         generator.generateSkipHeight(seed, pos, rand);
         generator.decorate();
-
         for (Pair<BPos, List<ItemStack>> chest : generator.getLoot()) {
-
+            System.out.println(chest.getFirst());
         }
     }
 }
